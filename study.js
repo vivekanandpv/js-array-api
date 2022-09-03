bad94d3e68fbc9c4a5954ad5fb8f71d35810f148
Vivekanand P V <vivekanandpv@outlook.com>
2022-09-03 19:04:14 +0530

    done

--- study.js
+++ study.js
@@ -1,19 +1,5 @@
 const scores = [9, 8, 7, 7, 5, 4, 9, 0];
 
-//  push adds the element at the end
-scores.push(10);
+//  slice produces a new subarray
 
-//  unshift adds the element at the beginning
-scores.unshift(67);
-
-//  splice to add at a random index
-scores.splice(3, 0, 34, 35, 36);
-
-//  pop to remove at then end (returns the removed element)
-const e1 = scores.pop(); //  e1 = 10
-
-//  shift to remove from the beginning (returns the removed element)
-const e2 = scores.shift(); //  e2 = 67;
-
-//  splice to remove at the random index
-scores.splice(3, 2); //  removes 2 elements starting from 3rd index
+const sa = scores.slice(3, 5); //  [7, 5] end index not inclusive
