29990ebc429505bb53e057bb9cde90a4cd57c8c4
Vivekanand P V <vivekanandpv@outlook.com>
2022-09-03 18:51:53 +0530

    done

--- study.js
+++ study.js
@@ -1,8 +1,13 @@
-//  Defining an array
+//  Arrays follow the reference semantics
+//  that means, the variable holds the reference to the actual array in the memory
+//  Therefore, the copy of the variable is a copy of the reference, not the copy of the array
+
 const scores = [9, 8, 7, 7, 5, 4, 9, 0];
 
-//  Arrays are objects
-console.log(typeof scores); //  object!
+const numbers = scores;
 
-//  Testing for array
-console.log(Array.isArray(scores)); //  true
+//  Now, numbers and scores are two references to the same array in the memory
+
+numbers[0] = 899;
+
+console.log(scores[0]); //  899
