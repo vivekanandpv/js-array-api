e99693238894bb646d26b1e61328260e66b2ebaf
Vivekanand P V <vivekanandpv@outlook.com>
2022-09-03 18:48:56 +0530

    done

---
+++ study.js
@@ -0,0 +1,8 @@
+//  Defining an array
+const scores = [9, 8, 7, 7, 5, 4, 9, 0];
+
+//  Arrays are objects
+console.log(typeof scores); //  object!
+
+//  Testing for array
+console.log(Array.isArray(scores)); //  true
