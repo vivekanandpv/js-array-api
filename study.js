8971f029b336de915189e048e834546b8df58b08
Vivekanand P V <vivekanandpv@outlook.com>
2022-09-03 19:06:27 +0530

    done

--- study.js
+++ study.js
@@ -1,5 +1,7 @@
 const scores = [9, 8, 7, 7, 5, 4, 9, 0];
 
-//  slice produces a new subarray
+//  join produces the comma-separated string representation of the elements
+const st = scores.join();
 
-const sa = scores.slice(3, 5); //  [7, 5] end index not inclusive
+//  concat produces a new concatenated array
+const na = scores.concat([99, 97, 76]);
