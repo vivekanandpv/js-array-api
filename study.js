f048e8f2f8ef39eeeb21f2f0a104552cfb1ec837
Vivekanand P V <vivekanandpv@outlook.com>
2022-09-03 19:10:33 +0530

    done

--- study.js
+++ study.js
@@ -1,7 +1,5 @@
 const scores = [9, 8, 7, 7, 5, 4, 9, 0];
 
-//  join produces the comma-separated string representation of the elements
-const st = scores.join();
-
-//  concat produces a new concatenated array
-const na = scores.concat([99, 97, 76]);
+//  sort can be natural or custom using comparator function
+//  original array is sorted
+scores.sort((a, b) => a - b);
