0dbfc4e15d9bba4b96ff3dbaca666bb596d94b8b
Vivekanand P V <vivekanandpv@outlook.com>
2022-09-03 19:14:38 +0530

    done

--- study.js
+++ study.js
@@ -1,5 +1,12 @@
 const scores = [9, 8, 7, 7, 5, 4, 9, 0];
 
-//  sort can be natural or custom using comparator function
-//  original array is sorted
-scores.sort((a, b) => a - b);
+//  map produces the new array by mapping every element
+const na1 = scores.map((i) => i * i);
+
+//  filter selects the element that pass the predicate
+const na2 = scores.filter((i) => i % 2 === 0);
+
+//  forEach calls the function with each element (iterator)
+scores.forEach((el, i) => console.log(`Element at ${i} is ${el}`));
+
+//  map, filter, forEach do not change the source array
